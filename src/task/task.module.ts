c08424c67f43111f0e3f1bf96e4d02ace2a77c44
Tom BRUAIRE <tombruaire@gmail.com>
2024-05-23 14:12:17 +0200

    Update modules (task and user)

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
 
-@Module({})
+@Module({
+    controllers: [TaskController],
+    providers: [TaskService],
+    exports: [TaskService]
+})
 export class TaskModule {}
