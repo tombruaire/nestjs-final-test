0dfd274ce2545e73c1b6c77840684807f5d091b4
Tom BRUAIRE <tombruaire@gmail.com>
2024-05-23 16:46:49 +0200

    Problèmes controllers user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import type { UserService } from './user.service';
 
-@Controller('user')
+@Controller('user/')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    //
+    @Post('add/:email')
+    async addUser(@Param('email') email: string): Promise<void> {
+        return this.userService.addUser(email);
+    }
+    
+    @Get(':email')
+    async getUser(
+        @Param('email') email: string
+    ): Promise<unknown> {
+        return this.userService.getUser(email);
+    }
+
+    @Get('reset-data')
+    async resetData(): Promise<void> {
+        return this.userService.resetData();
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,15 +3,15 @@ import { Injectable, NotImplementedException } from '@nestjs/common';
 @Injectable()
 export class UserService {
 
-    addUser(email: string): Promise<void> {
+    async addUser(email: string): Promise<void> {
         throw new NotImplementedException();
     }
 
-    getUser(email: string): Promise<unknown> {
+    async getUser(email: string): Promise<unknown> {
         throw new NotImplementedException();
     }
 
-    resetData(): Promise<void> {
+    async resetData(): Promise<void> {
         throw new NotImplementedException();
     }
 }
