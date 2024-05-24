81b483bf2be6e2d6d23161b3849e5e72a9e81136
Tom BRUAIRE <tombruaire@gmail.com>
2024-05-24 09:22:56 +0200

    Edit schema prisma et module prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { AppRoutingModule } from './app.routing-module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
     imports: [AppRoutingModule, ConfigurationModule, DatabaseModule],
+    controllers: [TaskController],
+    providers: [TaskService, PrismaService]
 })
 export class AppModule {}
