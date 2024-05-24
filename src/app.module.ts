a39767fffaebda33e540f41cd1a944c0a7041c05
Tom BRUAIRE <tombruaire@gmail.com>
2024-05-24 17:12:39 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,23 @@ import { AppRoutingModule } from './app.routing-module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [AppRoutingModule, ConfigurationModule, DatabaseModule]
+    imports: [
+        AppRoutingModule, 
+        ConfigurationModule, 
+        DatabaseModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'nestjs-final-test-db',
+            entities: [],
+            synchronize: true,
+          }),
+    ]
 })
 export class AppModule {}
